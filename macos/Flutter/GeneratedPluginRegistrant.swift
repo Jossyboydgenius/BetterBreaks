118eb012946f0dccbcc6d4de71e54ab954715c9e
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-05 17:26:44 +0000

    Remove Sentry plugin registration from platform-specific files
    - Deleted Sentry plugin registration in Linux, macOS, and Windows generated plugin registrant files
    - Removed Sentry from platform-specific plugin lists
    - Completed cleanup of Sentry integration across different platforms

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import connectivity_plus
 import package_info_plus
-import sentry_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
 }
