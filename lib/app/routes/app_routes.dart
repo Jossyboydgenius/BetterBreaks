3175319694340ee323c2739e764033cbb6be18b3
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-05 09:43:35 +0000

    Add SignUp route to AppRoutes configuration
    - Imported SignUpView in app_routes.dart
    - Added signUp route constant
    - Mapped signUp route to SignUpView in routes configuration

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,11 +3,13 @@ import 'package:better_breaks/ui/views/onboarding/onboarding_view.dart';
 import 'package:better_breaks/ui/views/onboarding/splash_screen_view.dart';
 import 'package:go_router/go_router.dart';
 import 'package:better_breaks/ui/views/auth/sign_in_view.dart';
+import 'package:better_breaks/ui/views/auth/sign_up_view.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String onboarding = '/onboarding';
   static const String signIn = '/sign-in';
+  static const String signUp = '/sign-up';
 
   static const String onboardingView = '/onboardingView';
   static const String splashScreenView = '/splashScreenView';
@@ -18,6 +20,7 @@ class AppRoutes {
     splashScreenView: (context) => const SplashScreenView(),
     onboardingView: (context) => const OnboardingView(),
     signIn: (context) => const SignInView(),
+    signUp: (context) => const SignUpView(),
   };
 
   static GoRouter router = GoRouter(
