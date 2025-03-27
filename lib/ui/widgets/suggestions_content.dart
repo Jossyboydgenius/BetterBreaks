57818451f20d196e0e3f099d7bd29e6f86a2eec6
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-27 07:26:55 +0000

    Add SuggestionsContent widget to display holiday suggestions

---
+++ lib/ui/widgets/suggestions_content.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:better_breaks/ui/widgets/suggestion_card.dart';
+import 'package:better_breaks/ui/views/planner/planner_view.dart';
+
+class SuggestionsContent extends StatelessWidget {
+  final VoidCallback? onBack;
+  
+  const SuggestionsContent({
+    super.key,
+    this.onBack,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SuggestionCard(
+          dateRange: 'December 27-29',
+          description: 'Take 3 days off to get 9 days of holiday',
+          isHighImpact: true,
+          holidays: ['Christmas', 'New year'],
+          onPreviewTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PlannerView(
+                  onBack: onBack,
+                  isSetup: true,
+                ),
+              ),
+            );
+          },
+        ),
+        SuggestionCard(
+          dateRange: 'November 10-20',
+          description: 'Take 3 days off to get 9 days of holiday',
+          isHighImpact: false,
+          holidays: ['Salah', 'El-fatir'],
+          onPreviewTap: () {
+            // Handle preview tap
+          },
+        ),
+        SuggestionCard(
+          dateRange: 'May 09-12',
+          description: 'Take 3 days off to get 9 days of holiday',
+          isHighImpact: false,
+          holidays: ['El-fatir', 'Salah'],
+          onPreviewTap: () {
+            // Handle preview tap
+          },
+        ),
+        SuggestionCard(
+          dateRange: 'May 09-12',
+          description: 'Take 3 days off to get 9 days of holiday',
+          isHighImpact: false,
+          holidays: ['El-fatir', 'Salah'],
+          onPreviewTap: () {
+            // Handle preview tap
+          },
+        ),
+      ],
+    );
+  }
+} 
