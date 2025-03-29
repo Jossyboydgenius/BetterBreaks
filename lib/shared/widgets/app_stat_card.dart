212c3a0887ab8c741daa18909d407ee91b903328
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-29 19:48:04 +0000

    Add AppStatCard widget for displaying statistics with customizable styles and layout, enhancing the visual representation of data in the application.

---
+++ lib/shared/widgets/app_stat_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:better_breaks/shared/app_textstyle.dart';
+import 'package:better_breaks/shared/app_icons.dart';
+
+class AppStatCard extends StatelessWidget {
+  final String title;
+  final String value;
+  final String iconData;
+  final Color color;
+  final double width;
+
+  const AppStatCard({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.iconData,
+    required this.color,
+    this.width = 120,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width.w,
+      padding: EdgeInsets.all(12.w),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 30.r,
+                height: 30.r,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: AppIcons(
+                    icon: iconData,
+                    color: color,
+                    size: 14.r,
+                  ),
+                ),
+              ),
+              SizedBox(width: 8.w),
+              Expanded(
+                child: Text(
+                  title,
+                  style: AppTextStyle.satoshi(
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 12.h),
+          Text(
+            value,
+            style: AppTextStyle.raleway(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
