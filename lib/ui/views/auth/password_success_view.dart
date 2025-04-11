e3e5de63b21dbe5e47e8a5511bcaf34012781399
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-11 04:27:35 +0000

    Add confetti animation to PasswordSuccessView for a celebratory effect upon password change. Refactored layout to use a Stack for improved visual presentation, maintaining existing success message and navigation button functionality.

--- lib/ui/views/auth/password_success_view.dart
+++ lib/ui/views/auth/password_success_view.dart
@@ -5,6 +5,7 @@ import 'package:better_breaks/shared/app_textstyle.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:better_breaks/ui/widgets/app_buttons.dart';
 import 'package:better_breaks/app/routes/app_routes.dart';
+import 'package:better_breaks/ui/widgets/confetti.dart';
 
 class PasswordSuccessView extends StatelessWidget {
   const PasswordSuccessView({super.key});
@@ -13,55 +14,70 @@ class PasswordSuccessView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(24.r),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Checkmark icon
-              AppIcons(
-                icon: AppIconData.success,
-                size: 120.r,
-              ),
-              SizedBox(height: 24.h),
-              
-              // Success message
-              Text(
-                "Password changed",
-                textAlign: TextAlign.center,
-                style: AppTextStyle.ralewayExtraBold48.copyWith(
-                  color: AppColors.lightBlack,
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Text(
-                    "Click on the login button below to continue.",
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.satoshiRegular20.copyWith(
-                      color: AppColors.grey700,
-                      ),
-                ),
-              ),
-              SizedBox(height: 32.h),
-              
-              // Back to login button
-              AppButton(
-                text: 'Login',
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    AppRoutes.signIn,
-                    (route) => false,
-                  );
-                },
-              ),
+      body: Stack(
+        children: [
+          // Add confetti widget
+          const Confetti(
+            confettiCount: 70,
+            colors: [
+              AppColors.primary,
+              AppColors.lightGreen,
+              AppColors.orange100,
             ],
           ),
-        ),
+
+          // Main content
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(24.r),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Checkmark icon
+                  AppIcons(
+                    icon: AppIconData.success,
+                    size: 120.r,
+                  ),
+                  SizedBox(height: 24.h),
+
+                  // Success message
+                  Text(
+                    "Password changed",
+                    textAlign: TextAlign.center,
+                    style: AppTextStyle.ralewayExtraBold48.copyWith(
+                      color: AppColors.lightBlack,
+                      fontSize: 24.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      "Click on the login button below to continue.",
+                      textAlign: TextAlign.center,
+                      style: AppTextStyle.satoshiRegular20.copyWith(
+                        color: AppColors.grey700,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 32.h),
+
+                  // Back to login button
+                  AppButton(
+                    text: 'Login',
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        AppRoutes.signIn,
+                        (route) => false,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
-} 
+}
