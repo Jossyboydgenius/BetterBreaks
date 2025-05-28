625b7c07653b96fa9b51bb123fd7e010bd878591
jossyboydgenius <jossyboydgenius@gmail.com>
2025-05-28 04:31:38 +0000

    Add AppPreferenceManager for managing app theme preferences using SharedPreferences

---
+++ lib/app/shared_preferences_manager.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppPreferenceManager {
+  static final AppPreferenceManager _instance =
+      AppPreferenceManager._internal();
+  static AppPreferenceManager get instance => _instance;
+
+  late SharedPreferences _prefs;
+
+  AppPreferenceManager._internal();
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  // Theme related methods
+  static const String _themeKey = 'app_theme_mode';
+
+  Future<void> saveThemeMode(int mode) async {
+    await _prefs.setInt(_themeKey, mode);
+  }
+
+  int getThemeMode() {
+    return _prefs.getInt(_themeKey) ?? 0; // 0 = system, 1 = light, 2 = dark
+  }
+}
