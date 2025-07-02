9260317a13e899925e436288cab8e32226d4dc38
jossyboydgenius <jossyboydgenius@gmail.com>
2025-07-02 02:30:21 +0000

    Refactor GlassyContainer to use AppThemeColors for improved theming consistency

--- lib/ui/widgets/glassy_container.dart
+++ lib/ui/widgets/glassy_container.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:ui';
+import 'package:better_breaks/shared/app_theme_colors.dart';
 
 class GlassyContainer extends StatelessWidget {
   final Widget child;
-  final Color backgroundColor;
-  final Color borderColor;
+  final Color? backgroundColor;
+  final Color? borderColor;
   final EdgeInsetsGeometry? padding;
   final double blurSigmaX;
   final double blurSigmaY;
@@ -13,12 +14,12 @@ class GlassyContainer extends StatelessWidget {
   final double? width;
   final double? height;
   final EdgeInsetsGeometry? margin;
-  
+
   const GlassyContainer({
     super.key,
     required this.child,
-    this.backgroundColor = Colors.white,
-    this.borderColor = Colors.white,
+    this.backgroundColor,
+    this.borderColor,
     this.padding,
     this.blurSigmaX = 10.0,
     this.blurSigmaY = 10.0,
@@ -30,6 +31,10 @@ class GlassyContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color bgColor =
+        backgroundColor ?? AppThemeColors.getCardColor(context);
+    final Color borderCol = borderColor ?? bgColor;
+
     return Container(
       width: width,
       height: height,
@@ -37,7 +42,7 @@ class GlassyContainer extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: borderRadius ?? BorderRadius.circular(16.r),
         border: Border.all(
-          color: borderColor.withOpacity(0.5),
+          color: borderCol.withOpacity(0.5),
           width: 1,
         ),
       ),
@@ -48,7 +53,7 @@ class GlassyContainer extends StatelessWidget {
           child: Container(
             padding: padding,
             decoration: BoxDecoration(
-              color: backgroundColor.withOpacity(0.45),
+              color: bgColor.withOpacity(0.45),
               borderRadius: borderRadius ?? BorderRadius.circular(16.r),
             ),
             child: child,
@@ -57,4 +62,4 @@ class GlassyContainer extends StatelessWidget {
       ),
     );
   }
-} 
+}
